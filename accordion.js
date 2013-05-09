105af012aeb3392295dbe084e193e5e80dc1c88f
Devindra Payment <Arden.ived@gmail.com>
2013-05-09 15:37:57 -0700

    Revert "Initial Commit"
    This reverts commit 0689a599638da28a5a434ae08d8dd5000c887d7a.

---
+++ accordion.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($) {
+	$.each(accordion_shortcode, function(id, attr) {
+		
+		$("#" + id).accordion(attr);
+	});
+	if (location.hash) {
+		$('a[href="'+location.hash+'"]').trigger('click');
+	}
+});
---
+++ support/twitter-bootstrap/action.php
@@ -0,0 +1,32 @@
+<?php
+
+
+
+function accordion_shortcode_twitter_bootstrap_atts( $atts, $selected ) {
+	
+	$atts['heading_link_attr'] = 'class="accordion-toggle" data-toggle="collapse" data-parent="#'.OLT_Accordion_Shortcode::$current_accordion_id.'"';
+	$atts['heading_tag']	= 'div';
+	$atts['heading_attr'] 	= ' class="accordion-heading" ';
+	
+	$atts['class'] 			= ($selected ? 'accordion-body collapse in' : 'accordion-body collapse' );
+	$atts['before_shell'] 	= '<div class="accordion-group">';
+	$atts['after_shell'] 	= '</div>';
+	$atts['before'] 		= '<div class="accordion-inner">';
+	$atts['after'] 			= '</div>';
+	
+	return $atts;
+
+}
+
+add_action( 'accordion-shortcode-atts', 'accordion_shortcode_twitter_bootstrap_atts', 10 , 2);
+
+
+function accordions_shortcode_twitter_bootstrap_atts( $atts ) {
+	
+	$atts['class'] 	= 'accordion';
+
+	return $atts;
+
+}
+
+add_action( 'accordions-shortcode-atts', 'accordions_shortcode_twitter_bootstrap_atts');
